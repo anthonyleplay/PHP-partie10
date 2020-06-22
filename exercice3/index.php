503abee0e5110bc773e960cdabe20f9b05525285
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-22 13:44:42 +0200

    preparation exo2 et 3

--- exercice3/index.php
+++ exercice3/index.php
@@ -7,16 +7,23 @@
     <title>TP 3</title>
 </head>
 
-<body>
-    <h1>TP 3</h1>
+<body class="bg-dark">
+    <div class="container bg-white my-3 rounded-lg">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="m-3 p-2 bg-light border rounded-lg">
+                    <h1>TP 3</h1>
+                    <p>Faire une fonction qui permet d'afficher les données des tableaux suivants :<br><br>
+                        &nbsp;&nbsp;&nbsp; $portrait1 = array('name'=&gt;'Victor', 'firstname'=&gt;'Hugo', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');<br>
+                        &nbsp;&nbsp;&nbsp; $portrait2 = array('name'=&gt;'Jean', 'firstname'=&gt;'de La Fontaine', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/e/e1/La_Fontaine_par_Rigaud.jpg');<br>
+                        &nbsp;&nbsp;&nbsp; $portrait3 = array('name'=&gt;'Pierre', 'firstname'=&gt;'Corneille', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');<br>
+                        &nbsp;&nbsp;&nbsp; $portrait4 = array('name'=&gt;'Jean', 'firstname'=&gt;'Racine', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');<br><br>
+                        Les afficher tous sur une même page.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <p>Faire une fonction qui permet d'afficher les données des tableaux suivants :<br><br>
-        &nbsp;&nbsp;&nbsp; $portrait1 = array('name'=&gt;'Victor', 'firstname'=&gt;'Hugo', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');<br>
-        &nbsp;&nbsp;&nbsp; $portrait2 = array('name'=&gt;'Jean', 'firstname'=&gt;'de La Fontaine', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/e/e1/La_Fontaine_par_Rigaud.jpg');<br>
-        &nbsp;&nbsp;&nbsp; $portrait3 = array('name'=&gt;'Pierre', 'firstname'=&gt;'Corneille', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');<br>
-        &nbsp;&nbsp;&nbsp; $portrait4 = array('name'=&gt;'Jean', 'firstname'=&gt;'Racine', 'portrait'=&gt;'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');<br><br>
-        Les afficher tous sur une même page.</p>
-    <hr>
 
 
 
